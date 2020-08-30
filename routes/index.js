d643ded1441d8c98d89ae24279b2fa0abe053d89
skandergod <thedanidacosta@gmail.com>
2020-08-30 08:17:43 -0400

    make it better

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var roster = require('./roster.json');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(roster.integrantes);
+  res.render('index', { title: 'Express', roster: roster.integrantes});
+});
+
+module.exports = router;
+
+//https://stackabuse.com/reading-and-writing-json-files-with-node-js/
