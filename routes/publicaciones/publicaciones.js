bc2578a92a1297e9794275b4b29d63b29181755c
skandergod <thedanidacosta@gmail.com>
2020-09-08 14:37:50 -0400

    planilla de publicaciones agregada

--- routes/publicaciones/publicaciones.js
+++ routes/publicaciones/publicaciones.js
@@ -9,4 +9,25 @@ router.get('/', function(req, res, next) {
 	res.render('publicaciones', {publicaciones: 1, publication: publications.publicaciones[0] ,roster: roster.integrantes});
 });
 
+router.get('/:id', function(req, res, next) {
+	console.log(req.params.id);
+	var act = lookForPublication(req.params.id);
+	console.log (act.imagenDePortada);
+	res.render('publicacion', {publicacion: 1, publication: act ,roster: roster.integrantes});
+});
+
+
+ // TODO: Mejorar búsqueda, actualmente O(n)
+function lookForPublication(titulo){
+	
+	for(var i = 0; i < publications.publicaciones.length; i++){
+		
+		if(publications.publicaciones[i].titulo.toLowerCase() == titulo){
+			return publications.publicaciones[i];
+		}
+	}
+
+	return false;
+}
+
 module.exports = router;
