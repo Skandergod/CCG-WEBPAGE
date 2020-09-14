7dca2b9e692fb876e4f16adb832fdc581f2ff6ac
adrianjmejias <adrianjmejias@gmail.com>
2020-09-14 17:36:22 -0400

    Passing prettier linter

--- routes/galerias/galerias.js
+++ routes/galerias/galerias.js
@@ -1,33 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var roster = require('../roster.json');
-var publications = require('../galerias/galerias.json');
+var roster = require("../roster.json");
+var publications = require("../galerias/galerias.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	//console.log(publications.publicaciones);
-	res.render('materias', {materias: 1, list: publications.publicaciones, act: publications.publicaciones[0], roster: roster.integrantes});
+router.get("/", function (req, res, next) {
+  //console.log(publications.publicaciones);
+  res.render("materias", {
+    materias: 1,
+    list: publications.publicaciones,
+    act: publications.publicaciones[0],
+    roster: roster.integrantes,
+  });
 });
 
-router.get('/:id', function(req, res, next) {
-	var act = lookForPublication(req.params.id);
-	//console.log (act.parrafos);
-	//console.log(act);
-	res.render('materia', {materia: 1, publication: act, roster: roster.integrantes});
+router.get("/:id", function (req, res, next) {
+  var act = lookForPublication(req.params.id);
+  //console.log (act.parrafos);
+  //console.log(act);
+  res.render("materia", {
+    materia: 1,
+    publication: act,
+    roster: roster.integrantes,
+  });
 });
 
+// TODO: Mejorar búsqueda, actualmzente O(n)
+function lookForPublication(titulo) {
+  for (var i = 0; i < publications.publicaciones.length; i++) {
+    if (publications.publicaciones[i].titulo == titulo) {
+      return publications.publicaciones[i];
+    }
+  }
 
- // TODO: Mejorar búsqueda, actualmzente O(n)
-function lookForPublication(titulo){
-	
-	for(var i = 0; i < publications.publicaciones.length; i++){
-		
-		if(publications.publicaciones[i].titulo == titulo){
-			return publications.publicaciones[i];
-		}
-	}
-
-	return false;
+  return false;
 }
 
 module.exports = router;
--- routes/publicaciones/publicaciones.js
+++ routes/publicaciones/publicaciones.js
@@ -1,33 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var roster = require('../roster.json');
-var publications = require('../publicaciones/publicaciones.json');
+var roster = require("../roster.json");
+var publications = require("../publicaciones/publicaciones.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	//console.log(publications.publicaciones);
-	res.render('publicaciones', {publicaciones: 1, list: publications.publicaciones, act: publications.publicaciones[0], roster: roster.integrantes});
+router.get("/", function (req, res, next) {
+  //console.log(publications.publicaciones);
+  res.render("publicaciones", {
+    publicaciones: 1,
+    list: publications.publicaciones,
+    act: publications.publicaciones[0],
+    roster: roster.integrantes,
+  });
 });
 
-router.get('/:id', function(req, res, next) {
-	var act = lookForPublication(req.params.id);
-	//console.log (act.parrafos);
-	console.log(act);
-	res.render('publicacion', {publicacion: 1, publication: act, roster: roster.integrantes});
+router.get("/:id", function (req, res, next) {
+  var act = lookForPublication(req.params.id);
+  //console.log (act.parrafos);
+  console.log(act);
+  res.render("publicacion", {
+    publicacion: 1,
+    publication: act,
+    roster: roster.integrantes,
+  });
 });
 
+// TODO: Mejorar búsqueda, actualmente O(n)
+function lookForPublication(titulo) {
+  for (var i = 0; i < publications.publicaciones.length; i++) {
+    if (publications.publicaciones[i].titulo == titulo) {
+      return publications.publicaciones[i];
+    }
+  }
 
- // TODO: Mejorar búsqueda, actualmente O(n)
-function lookForPublication(titulo){
-	
-	for(var i = 0; i < publications.publicaciones.length; i++){
-		
-		if(publications.publicaciones[i].titulo == titulo){
-			return publications.publicaciones[i];
-		}
-	}
-
-	return false;
+  return false;
 }
 
 module.exports = router;
