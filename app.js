5e0224e3b9d5db42cdceb7d38219e2a5b3ccfdc6
adrianjmejias <adrianjmejias@gmail.com>
2020-09-14 17:52:41 -0400

    Adding nodemon and listening on port message

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var publicacionesRouter = require('./routes/publicaciones/publicaciones');
-var proyectosRouter = require('./routes/proyectos/proyectos');
-var materiasRouter = require('./routes/materias/materias');
-var galeriasRouter = require('./routes/galerias/galerias');
-var roster = require('./routes/roster.json');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var publicacionesRouter = require("./routes/publicaciones/publicaciones");
+var proyectosRouter = require("./routes/proyectos/proyectos");
+var materiasRouter = require("./routes/materias/materias");
+var galeriasRouter = require("./routes/galerias/galerias");
+var roster = require("./routes/roster.json");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/publicaciones', publicacionesRouter);
-app.use('/proyectos', proyectosRouter);
-app.use('/materias', materiasRouter);
-app.use('/materias', galeriasRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/publicaciones", publicacionesRouter);
+app.use("/proyectos", proyectosRouter);
+app.use("/materias", materiasRouter);
+app.use("/materias", galeriasRouter);
 
-app.use('/css', express.static(__dirname + '/public/stylesheets'));
-app.use('/js', express.static(__dirname + '/public/javascripts'));
-app.use('/assets', express.static(__dirname + '/public/images'));
+app.use("/css", express.static(__dirname + "/public/stylesheets"));
+app.use("/js", express.static(__dirname + "/public/javascripts"));
+app.use("/assets", express.static(__dirname + "/public/images"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
