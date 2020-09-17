070765cafc7d4766e5ed01d4ed21854445770975
adrianjmejias <adrianjmejias@gmail.com>
2020-09-17 13:45:13 -0400

    Changing layout structure, footer is now at the bottom. Small bug on layout when content is bigger thatn page

--- routes/galeria/galeria.js
+++ routes/galeria/galeria.js
@@ -1,25 +1,26 @@
 var express = require("express");
 var router = express.Router();
-var roster = require('../roster.json');
-var publications = require('./galeria.json');
+var roster = require("../roster.json");
+var publications = require("./galeria.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	//console.log(publications.publicaciones);
-	res.render('galeria', {galeria: 1, list: publications.album, roster: roster.integrantes});
+router.get("/", function (req, res, next) {
+  //console.log(publications.publicaciones);
+  res.render("galeria", {
+    list: publications.album,
+    roster: roster.integrantes,
+  });
 });
 
- // TODO: Mejorar búsqueda, actualmzente O(n)
-function lookForPublication(titulo){
-	
-	for(var i = 0; i < publications.publicaciones.length; i++){
-		
-		if(publications.publicaciones[i].titulo == titulo){
-			return publications.publicaciones[i];
-		}
-	}
+// TODO: Mejorar búsqueda, actualmzente O(n)
+function lookForPublication(titulo) {
+  for (var i = 0; i < publications.publicaciones.length; i++) {
+    if (publications.publicaciones[i].titulo == titulo) {
+      return publications.publicaciones[i];
+    }
+  }
 
-	return false;
+  return false;
 }
 
 module.exports = router;
